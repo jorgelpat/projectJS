7a18b09130514ba104504e1ab7fdbc7cd1cfceb9
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-05-15 12:55:06 -0500

    feat: :construction: agregar dom de tracks y album

--- js/main.js
+++ js/main.js
@@ -64,6 +64,44 @@ const verAlbum = async (codeAlbum) => {
             // 'Content-Type': 'application/json'
         }
     };
+    try{
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const albums = result.albums.items;
+        const dcAs = data.coverArt.sources;
+        listAlbum.innerHTM = '';
+        for (let i = 0; i<albums.length; i++){
+            const getImage = albums[i]?.dcAs[i]?.url;
+            const firstImage = albums[i]?.dcAs[0]?.url;
+            const imagen = getImage ?? firstImage;
+            const nombre = albums[i].data.name;
+            const nombreArtista = albums[i].data.artists.items[i]?.profile.name ?? albums[i].data.artists.items[0]?.profile.name;
+            const fecha = albums[i].data.date.year;
+            const uri = albums[i].data.uri;
+
+            const div = document.createElement("div");
+            div.classList.add("albumes");         // Buscar donde agregarlo
+            div.innerHTML = `
+                <div class="album_order" data-id="${uri}">
+                    <div class="imagen_album">
+                        <img src="${imagen}" alt="" class="portada">
+                    </div>
+                    <div class="info_album">
+                        <h3>${nombre}</h3>
+                        <p>${nombreArtista}</p>
+                        <p>${fecha}</p>
+                    </div>
+                </div>
+            `;
+            listAlbum.append(div);
+            div.querySelector('.album_order').addEventListener('click',()=>{
+                const frame = document.querySelector("my.frame");
+                frame.setAttribute("uri",uri);
+            })
+        }
+    } catch (error) {
+        console.error(error);
+    }
     
 };
 
@@ -85,37 +123,70 @@ const verAlbum = async (codeAlbum) => {
 try {
 	const response = await fetch(url, options);
 	const result = await response.json();
-    let va = result.albums.items
-    for(let i = 0; i < 1000; i++){
-        fetch (url)
-        .then(response => response.json())
-        .then(data => mostrarAlbum(data))
-        let imagen = va[i]?.data.coverArt.sources[i]?.url ?? va[i]?.data.coverArt.sources[0]?.url;
-        let nombre = va[i].data.name
-        let nombreArtista = va[i].data.artists.items[i]?.profile.name ?? va[i].data.artists.items[0]?.profile.name;
-        let fecha = va[i].data.date.year
-        
-        const mostrarAlbum = async() => {
-            const div = document.createElement("div");
-            div.classList.add("album")
-            div.innerHTML = `
-            <div class="album">
-                <div class="album_order">
-                    <div class="imagen_album">
-                        <img src="${imagen}" alt="" class="portada">
-                    </div>
-                    <div class="info_album">
-                        <h3>${nombre}</h3>
-                        <p>${nombreArtista}</p>
-                        <p>${fecha}</p>
-                    </div>
+    let tracks = result.tracks.items
+    console.log(tracks)
+    for (let i = 0; i < 10; i++) {
+        const getImage = tracks[i]?.albums.images.url;
+        const firstImage = tracks[i]?.albums.images.url;
+        const imagen = getImage ?? firstImage;
+        const nombre = tracks[i].name;
+        const nombreArtista = tracks[i].artists.items[0].name
+        const uri = tracks[i].uri;
+
+        const div = document.createElement("div");
+        div.classList.add("trackRecomendations");           // Buscar donde agregarlo
+        div.innerHTML = `
+            <div class="track_order" data-id="${uri}">
+                <div class="imagen_track">
+                    <img src="${imagen}" alt="" class="portada">
                 </div>
-            </div>`;
-            listAlbum.append(div);
-        }
-        if(imagen){
-        }
+                <div class="info_track">
+                    <h3>${nombre}</h3>
+                    <p>${nombreArtista}</p>
+                </div>
+            </div>
+        `;
     }
 } catch (error) {
 	console.error(error);
 }
+
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.json();
+//     let va = result.tracks.items
+//     console.log(tracks)
+//     for(let i = 0; i < 1000; i++){
+//         fetch (url)
+//         .then(response => response.json())
+//         .then(data => mostrarAlbum(data))
+//         let imagen = va[i]?.data.coverArt.sources[i]?.url ?? va[i]?.data.coverArt.sources[0]?.url;
+//         let nombre = va[i].data.name
+//         let nombreArtista = va[i].data.artists.items[i]?.profile.name ?? va[i].data.artists.items[0]?.profile.name;
+//         let fecha = va[i].data.date.year
+        
+//         const mostrarAlbum = async() => {
+//             const div = document.createElement("div");
+//             div.classList.add("album")
+//             div.innerHTML = `
+//             <div class="album">
+//                 <div class="album_order">
+//                     <div class="imagen_album">
+//                         <img src="${imagen}" alt="" class="portada">
+//                     </div>
+//                     <div class="info_album">
+//                         <h3>${nombre}</h3>
+//                         <p>${nombreArtista}</p>
+//                         <p>${fecha}</p>
+//                     </div>
+//                 </div>
+//             </div>`;
+//             listAlbum.append(div);
+//         }
+//         if(imagen){
+//         }
+//     }
+// } catch (error) {
+// 	console.error(error);
+// }
